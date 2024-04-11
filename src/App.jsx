830b0f7d4dc489f5754ffba17071d0fd754eb8b3
Pranav Trivedi <81945998+pranavppt@users.noreply.github.com>
2024-04-11 14:34:01 +0000

    first changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Pranav Trivedi",
+  title: "Web Designer & Software developer",
+  email: "",
+  gitHub: "github.com/pranavppt",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/pranavppt",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/ppt_here",
+  youTube: "Npe",
 };
 
 const primaryColor = "#4E567E";
